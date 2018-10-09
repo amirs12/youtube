464435b3bcc5815001ad636f8f0617878450894c
Inbal <Inbal@Inbals-MacBook-Air.local>
2018-10-09 18:30:10 +0300

    with mlab

--- servers/db.js
+++ servers/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-let DB_URL = `mongodb://admin:admin12@ds123603.mlab.com:23603/users-list`
+//const DB_URL = `mongodb://admin:admin12@ds123603.mlab.com:23603/users-list`
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/fed-mongoose';
 
 mongoose.connect(DB_URL);
 
